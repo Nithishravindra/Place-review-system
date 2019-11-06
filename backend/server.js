453de4955d0799a38dc370244d6bc0fbb301d82a
Satya Rohith <me@satyarohith.com>
2019-11-07 02:51:06 +0530

    feat: add a temp token to demonstrate

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,25 @@
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const userRoutes = require('./routes/user');
 const placeRoutes = require('./routes/place');
 const ratingRoutes = require('./routes/rating');
 const feedback = require('./routes/feedback');
-const cors = require("cors");
-const session = require('express-session')
 
-var app = express();
+// Remove this and put it in .env file.
+process.env.JWT_SECRET = 'a_secret_aint_a_secret_when_git_pushed';
+
+const app = express();
+
 app.use(cors());
-
 app.use(bodyParser.json());
-app.use(session({
-    secret: '2C44-4D44-WppQ38S',
-    resave: true,
-    saveUninitialized: true
-}));
-app.use("/users", userRoutes);
-app.use("/places",placeRoutes);
-app.use("/rating",ratingRoutes);
-app.use("/feedback", feedback);
 
-app.listen(3000, () => {console.log('Server running on port 3000')});
+app.use('/users', userRoutes);
+app.use('/places', placeRoutes);
+app.use('/rating', ratingRoutes);
+app.use('/feedback', feedback);
+
+app.listen(3000, () => {
+  console.log('Server running on port http://localhost:3000');
+});
